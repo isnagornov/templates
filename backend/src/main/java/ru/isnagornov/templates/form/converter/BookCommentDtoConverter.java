65ef1d29056d4eb366684ff624f49fabfb7a844d
isnagornov <isnagornov@yandex.ru>
2019-09-03 10:31:30 +0300

    books rating, fix mapper

--- backend/src/main/java/ru/isnagornov/templates/form/converter/BookCommentDtoConverter.java
+++ backend/src/main/java/ru/isnagornov/templates/form/converter/BookCommentDtoConverter.java
@@ -1,8 +1,5 @@
 package ru.isnagornov.templates.form.converter;
 
-import ma.glasnost.orika.CustomMapper;
-import ma.glasnost.orika.MappingContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.isnagornov.templates.entity.BookComment;
 import ru.isnagornov.templates.form.BookCommentForm;
@@ -10,26 +7,7 @@ import ru.isnagornov.templates.form.BookCommentForm;
 @Component
 public class BookCommentDtoConverter extends BaseConverter<BookComment, BookCommentForm> {
 
-    @Autowired
-    private BookDtoConverter bookDtoConverter;
-
     public BookCommentDtoConverter() {
         super(BookComment.class, BookCommentForm.class);
-
-        getClassMapBuilder().customize(new CustomMapper<BookComment, BookCommentForm>() {
-            @Override
-            public void mapAtoB(BookComment bookComment, BookCommentForm bookCommentForm, MappingContext context) {
-                super.mapAtoB(bookComment, bookCommentForm, context);
-                bookComment.setBook(bookDtoConverter.getEntity(bookCommentForm.getBook()));
-            }
-
-            @Override
-            public void mapBtoA(BookCommentForm bookCommentForm, BookComment bookComment, MappingContext context) {
-                super.mapBtoA(bookCommentForm, bookComment, context);
-                bookCommentForm.setBook(bookDtoConverter.getDto(bookComment.getBook()));
-
-            }
-
-        }).byDefault().register();
     }
 }
